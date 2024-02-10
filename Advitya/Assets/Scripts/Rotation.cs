3042a95cc958599d23a5e7a46d5c9e9085fa4ba9
ShivangNagta <shivangnag@gmail.com>
2024-02-11 02:31:49 +0530

    Login
    Login added

---
+++ Advitya/Assets/Scripts/Rotation.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotation : MonoBehaviour
+{
+    public Vector3 rot;
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(rot * Time.deltaTime);
+    }
+}
